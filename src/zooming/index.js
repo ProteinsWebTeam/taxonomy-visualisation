848effe3c2f826f376ee1698124cae84517f17cf
Gustavo A. Salazar <gustavoadolfo.salazar@gmail.com>
2019-03-06 17:06:32 +0000

    limiting zooming and paning

--- src/zooming/index.js
+++ src/zooming/index.js
@@ -2,14 +2,29 @@ import { zoom, event } from 'd3';
 export default (treeSelection, global) => {
   let tmpX = global.margin;
   let tmpY = 0;
+
   const zooming = () => {
+    const { width, height } = treeSelection.node().getBoundingClientRect();
     if (event.sourceEvent.movementX) tmpX += event.sourceEvent.movementX;
     if (event.sourceEvent.movementY) tmpY += event.sourceEvent.movementY;
-    global.scale = event.transform.k;
-    const { width, height } = treeSelection.node().getBoundingClientRect();
+    const k = (global.scale = event.transform.k);
+    const edges = {
+      right: width - global.margin,
+      left: k * (-width + 10 * global.margin),
+      top: k * (-height + 4 * global.margin),
+      bottom: height - 4 * k * global.margin,
+    };
+    if (tmpX > edges.right) tmpX = edges.right;
+    if (tmpX < edges.left) tmpX = edges.left;
+    if (tmpY > edges.bottom) tmpY = edges.bottom;
+    if (tmpY < edges.top) tmpY = edges.top;
     treeSelection.attr('viewBox', `${-tmpX} ${-tmpY} ${width} ${height}`);
     global.instance.redraw();
   };
 
-  treeSelection.call(zoom().on('zoom', zooming));
+  treeSelection.call(
+    zoom()
+      .on('zoom', zooming)
+      .scaleExtent([1, 5])
+  );
 };
